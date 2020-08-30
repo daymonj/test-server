f6ee0b2532cf73987c59bd44adfa373cb47c9e36
psyeon990 <psyeon990@naver.com>
2020-08-30 14:39:37 +0900

    server initial setting

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/hoho', function(req, res, next) {
+  res.json({name:'jaeyun',age:27});
+});
+
+
+
+module.exports = router;
